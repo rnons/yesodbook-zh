effff425b4ed65eb56245e02e64b4b3c395d9193
rnons <remotenonsense@gmail.com>
2014-01-18 21:51:11 +0800

    Generate html site from asciidoc files.

---
+++ generate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+rm -rf html
+mkdir html
+for f in chapters/*.asciidoc
+do
+    BN=`basename $f`
+    DEST=html/${BN%.asciidoc}.html
+    asciidoc -b html5 -f layout.conf -a toc -o $DEST $f
+done
